e376a4202822f8b342af0401bfdd1f0381edb087
Fangwei Jiang <emberspirit2018@gmail.com>
2023-08-22 00:01:43 +1000

    update index.js

--- index.js
+++ index.js
@@ -2,25 +2,32 @@ const apikey = "46f80a02ecae410460d59960ded6e1c6",
     city = document.getElementById("city-input"),
     weatherData = document.getElementById("weather-data");
 
+function clearInput() {
+    weatherData.querySelector(".icon").innerHTML = "";
+    weatherData.querySelector(".details").innerHTML = "";
+    weatherData.querySelector(".temperature").textContent = "";
+    weatherData.querySelector(".description").textContent = "";
+}
+
 async function getWeather() {
     try {
         const response = await axios.get(`//api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apikey}&units=metric`);
         const data = response.data;
-        if (response.status !== 200) throw new Error("Network response error");
-        else {
-            console.log("data", data);
-            weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="not found"/>`;
-            weatherData.querySelector(".temperature").textContent = `${Math.round(data.main.temp)}°C`;
-            weatherData.querySelector(".description").textContent = `${data.weather[0].description}`;
-            weatherData.querySelector(".details").innerHTML = [`Feels like: ${Math.round(data.main.feels_like)}`, `humidity: ${data.main.humidity}`, `pressure: ${data.main.pressure}`]
-                .map(value => `<div>${value}</div>`)
-                .join("");
-        }
+        if (city.value === "") clearInput();
+        setTimeout(() => {
+            if (response.status !== 200) throw new Error("Network response error");
+            else {
+                console.log("data", data);
+                weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="not found"/>`;
+                weatherData.querySelector(".temperature").textContent = `${Math.round(data.main.temp)}°C`;
+                weatherData.querySelector(".description").textContent = `${data.weather[0].description}`;
+                weatherData.querySelector(".details").innerHTML = [`Feels like: ${Math.round(data.main.feels_like)}`, `humidity: ${data.main.humidity}`, `pressure: ${data.main.pressure}`]
+                    .map(value => `<div>${value}</div>`)
+                    .join("");
+            }
+        }, 200);
     } catch (err) {
-        weatherData.querySelector(".icon").innerHTML = "";
-        weatherData.querySelector(".details").innerHTML = "";
-        weatherData.querySelector(".temperature").textContent = "";
-        weatherData.querySelector(".description").textContent = "";
+        clearInput();
     }
 }
 city.focus();
